b4565af6e169bf7e5f0327f5ad2093439260f771
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2023-01-20 15:28:06 -0600

    Change Success message

--- components/Form.jsx
+++ components/Form.jsx
@@ -166,7 +166,7 @@ const Form = () => {
                     </div>
                     <button aria-label="Sumbit button" type="submit" className=" block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Submit</button>
                 </form>
-                : <h2>Thank You For your Submittion</h2>
+                : <h2>Thanks for your Submittion</h2>
                 }
                 
             </div>
