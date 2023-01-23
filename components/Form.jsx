901843123f6846d31b5d4b1da6284e1425be33e1
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2023-01-23 16:26:45 -0600

    change Fetch to axios only; Test todo

--- components/Form.jsx
+++ components/Form.jsx
@@ -16,7 +16,7 @@ const Form = () => {
 
 useEffect(() => {
 
-        axios.get(Url,)
+        axios.get(`${Url}`,)
             .then(response => {
                 setFetch_Form_API_Results(response.data)
 
@@ -68,25 +68,21 @@ const handleChange = (e) => {
 
 
 
+
+
     const handleSubmit = async (e) => {
     // prevent multiple post request
-        e.preventDefault();
-        
-        isPosted(false)
-
-        const response = await fetch(`${Url}`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-
-        if (response.status == 201) {
-            isPosted(true)
-
-        }
+    e.preventDefault();
+    isPosted(false)
+    
+    try {
+      const response = await axios.post(`${Url}`, data)
+      if (response.status === 201) {
+        isPosted(true)
+      }
+    } catch (error) {
+    //   console.log("Error:", error)
+    }
     }
 
 
@@ -102,7 +98,7 @@ const handleChange = (e) => {
                         connect with friends in the Fetch app!</p>
                         {/* the Link brings you to the landing page of fetch Rewards */}
                     <Link href={`https://fetch.com/`} target='_blank'>
-                        <button className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Learn More</button>
+                        <button className="transition delay-150 duration-300 ease-in-out block w-28 bg-white hover:bg-yellow-400 text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Learn More</button>
                     </Link>
                 </div>
                 {/* small figures on top */}
@@ -117,14 +113,14 @@ const handleChange = (e) => {
                 {!Posted ?
                 <form onSubmit={handleSubmit} data-testid="Form">
                     <h1 className="text-black font-bold text-2xl mb-1">Hi There!</h1>
-                    <p className="text-sm font-normal text-black mb-7">Welcome Back</p>
+                    <p className="text-sm font-normal text-black mb-7">Welcome</p>
                     <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 border-black">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" viewBox="0 0 20 20"
                             fill="currentColor">
                             <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                                 clipRule="evenodd" />
                         </svg>
-                        <label htmlFor="name">Name</label>
+                        {/* <label htmlFor="name">Name</label> */}
                         <input data-testid="name-input" onChange={handleChange} className="pl-2 outline-none border-none bg-transparent text-black" type="text" name="name" value={data.name} placeholder="Full name" maxLength="50" minLength='3' aria-required="true" required />
                     </div>
                     <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 border-black">
@@ -133,7 +129,7 @@ const handleChange = (e) => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                 d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                         </svg>
-                        <label htmlFor="emal">Email</label>
+                        {/* <label htmlFor="emal">Email</label> */}
                         <input data-testid="email-input" onChange={handleChange} className="pl-2 outline-none border-none bg-transparent text-black" type="email" name="email" value={data.email} placeholder="Email Address" maxLength="50" minLength='3' aria-required="true" required />
                     </div>
                     <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 border-black">
@@ -143,7 +139,7 @@ const handleChange = (e) => {
                                 d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                                 clipRule="evenodd" />
                         </svg>
-                        <label htmlFor="password">Password</label>
+                        {/* <label htmlFor="password">Password</label> */}
                         <input data-testid="password-input" onChange={handleChange} className="pl-2 outline-none border-none bg-transparent text-black" type="password" name="password" value={data.password} placeholder="Password" maxLength="50" minLength='3' aria-required="true" required />
                     </div>
                     <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 border-black">
@@ -152,9 +148,9 @@ const handleChange = (e) => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                 d="M5,22H19a3,3,0,0,0,3-3V9a3,3,0,0,0-3-3H17V4a2,2,0,0,0-2-2H9A2,2,0,0,0,7,4V6H5A3,3,0,0,0,2,9V19A3,3,0,0,0,5,22ZM9,4h6V6H9ZM4,9A1,1,0,0,1,5,8H19a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1Zm2,3a1,1,0,0,1,1-1H17a1,1,0,0,1,0,2H7A1,1,0,0,1,6,12Zm0,4a1,1,0,0,1,1-1H17a1,1,0,0,1,0,2H7A1,1,0,0,1,6,16Z" />
                         </svg>
-                        <label htmlFor="occupation"/>
+                        {/* <label htmlFor="occupation"/> */}
                         <select data-testid="occupation-select" onChange={handleChange} className="pl-2 outline-none border-none bg-transparent text-black" type="text" name="occupation" value={data.occupation} placeholder="Occupation" maxLength="50" minLength='3' aria-required="true" required>
-                            <option data-testid="" defaultValue>Choose an Occupation</option>
+                            {/* <option data-testid="" defaultValue>Choose an Occupation</option> */}
                             {theOccupations}
                         </select>
                     </div>
@@ -164,15 +160,15 @@ const handleChange = (e) => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                 d="M11.3856 23.789L11.3831 23.7871L11.3769 23.7822L11.355 23.765C11.3362 23.7501 11.3091 23.7287 11.2742 23.7008C11.2046 23.6451 11.1039 23.5637 10.9767 23.4587C10.7224 23.2488 10.3615 22.944 9.92939 22.5599C9.06662 21.793 7.91329 20.7041 6.75671 19.419C5.60303 18.1371 4.42693 16.639 3.53467 15.0528C2.64762 13.4758 2 11.7393 2 10C2 7.34784 3.05357 4.8043 4.92893 2.92893C6.8043 1.05357 9.34784 0 12 0C14.6522 0 17.1957 1.05357 19.0711 2.92893C20.9464 4.8043 22 7.34784 22 10C22 11.7393 21.3524 13.4758 20.4653 15.0528C19.5731 16.639 18.397 18.1371 17.2433 19.419C16.0867 20.7041 14.9334 21.793 14.0706 22.5599C13.6385 22.944 13.2776 23.2488 13.0233 23.4587C12.8961 23.5637 12.7954 23.6451 12.7258 23.7008C12.6909 23.7287 12.6638 23.7501 12.645 23.765L12.6231 23.7822L12.6169 23.7871L12.615 23.7885C12.615 23.7885 12.6139 23.7894 12 23L12.6139 23.7894C12.2528 24.0702 11.7467 24.0699 11.3856 23.789ZM12 23L11.3856 23.789C11.3856 23.789 11.3861 23.7894 12 23ZM15 10C15 11.6569 13.6569 13 12 13C10.3431 13 9 11.6569 9 10C9 8.34315 10.3431 7 12 7C13.6569 7 15 8.34315 15 10Z" />
                         </svg>
-                        <label htmlFor="states"/>
+                        {/* <label htmlFor="states"/> */}
                         <select data-testid="state-select" onChange={handleChange} className="pl-2 outline-none border-none bg-transparent text-black" type="text" name="state" value={data.state[0]} placeholder="State" maxLength="50" minLength='3' aria-required="true" required>
-                            <option data-testid="OptionForState"  defaultValue>Choose a State</option>
+                            {/* <option data-testid="OptionForState"  defaultValue>Choose a State</option> */}
                             {theStates}
                         </select>
                     </div>
-                    <button data-testid="submit-button" aria-label="Sumbit button" type="submit" className=" block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Submit</button>
+                    <button data-testid="submit-button" aria-label="Sumbit button" type="submit" className="transition delay-150 duration-300 ease-in-out block w-full bg-indigo-600 hover:bg-black mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Submit</button>
                 </form>
-                : <h2 data-testid="SuccessMessage">Thanks for your Submittion</h2>
+                : <h2 data-testid="success-message">Thanks for your Submittion</h2>
                 }
                 
             </div>
--- components/Form.test.js
+++ components/Form.test.js
@@ -1,107 +1,13 @@
-
-// TODO: Work on the testing/ Status: Not working as planned.
-
-
-
-
-import { render, fireEvent, act, wait } from '@testing-library/react'
+import React from 'react'
+import { render, fireEvent, wait } from '@testing-library/react'
+import axiosMock from 'axios'
 import Form from './Form'
 
-jest.mock('axios', () => {
-    return {
-        get: jest.fn().mockResolvedValue({
-            data: {
-                occupations: ['Developer', 'Designer', 'Product Manager'],
-                states: [{ name: 'New York', abbreviation: 'NY' }, { name: 'California', abbreviation: 'CA' }]
-            }
-        }),
-        post: jest.fn().mockResolvedValue({ status: 201 })
-    }
-})
 
-import axios from 'axios'
 
 describe('Form', () => {
-    test('renders the form and options when the data is fetched', async () => {
-        // mock the fetch API
-        act(() => {
-            axios.get(Url,)
-            .then(response => {
-                setFetch_Form_API_Results(response.data)
-            })
-        }, [Url]);
-    
-        const { getByTestId } = render(<Form />)
-    
-        // wait for the data to be fetched
-        await wait(() => {
-            const form = getByTestId('form')
-            expect(form).toBeInTheDocument()
-    
-            const occupationSelect = getByTestId('occupation-select')
-            expect(occupationSelect.options.length).toBe(3)
-    
-            const stateSelect = getByTestId('state-select')
-            expect(stateSelect.options.length).toBe(2)
-        })
-    })
-    
-
-    test('submits the form', async () => {
-        const { getByTestId } = render(<Form />)
-
-        // fill in the form inputs
-        const nameInput = getByTestId('name-input')
-        fireEvent.change(nameInput, { target: { value: 'John Doe' } })
-
-        const emailInput = getByTestId('email-input')
-        fireEvent.change(emailInput, { target: { value: 'johndoe@example.com' } })
-
-        const passwordInput = getByTestId('password-input')
-        fireEvent.change(passwordInput, { target: { value: 'password123' } })
-
-        // submit the form
-        const submitButton = getByTestId('submit-button')
-        fireEvent.click(submitButton)
-
-        // assert the fetch API was called with the correct data
-        expect(axios.post).toHaveBeenCalledWith(
-            expect.any(String),
-            expect.objectContaining({
-                name: 'John Doe',
-                email: 'johndoe@example.com',
-                password: 'password123',
-                occupation: '',
-                state: []
-            })
-        )
-
-        // assert the form was submitted successfully
-        const message = getByText(/Thanks for submitting!/i)
-        expect(message).toBeInTheDocument()
+    test('renders form and submits data', async () => {
+        // set up the mock response for the GET request
     })
 
-    test('selects an occupation', () => {
-        const { getByTestId } = render(<Form />)
-        const occupationSelect = getByTestId('occupation-select')
-
-        // select the "Developer" option
-        fireEvent.change(occupationSelect, { target: { value: 'Developer' } })
-
-        // assert the selected value is "Developer"
-        expect(occupationSelect.value).toBe('Developer')
-    })
-    test('selects state', () => {
-        const { getByTestId } = render(<Form />)
-        const stateSelect = getByTestId('state-select')
-
-
-        // select the "New York" option
-        fireEvent.change(stateSelect, { target: { value: { name: "New York", abbreviation: "NY" } } })
-
-        // assert the selected value is "New York"
-        expect(stateSelect.value).toBe({ name: "New York", abbreviation: "NY" })
-    })
-
-    //end
 })
