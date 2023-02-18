002c07fe597c9c0cbf0fcbb18db0e597c16f45b4
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2023-02-18 15:16:48 -0600

    Testing work for the Form and FormInput.Rewrite some code inside FormInput

--- components/Form.test.js
+++ components/Form.test.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { render, fireEvent, wait } from '@testing-library/react'
-import axiosMock from 'axios'
+import {render, screen} from '@testing-library/react'
+import {user} from '@testing-library/user-event'
 import Form from './Form'
+import '@testing-library/jest-dom'
 
 
 
-describe('Form', () => {
-    test('renders form and submits data', async () => {
-        // set up the mock response for the GET request
-    })
+test('It should have a Heading, paragraph and a button', () =>{
+    
+    //Render
+    render(<Form/>)
 
+    // Manipulation
+
+    const heading = screen.getByRole('heading', { name: /fetchrewards/i, hidden: true });
+    const paragraph = screen.getByText(/snap receipts, earn rewards and connect with friends in the fetch app!/i);
+    const link =  screen.getByRole('link', { name: /learn more/i, hidden: true });
+    const button = screen.getByRole('button', { name: /learn more/i, hidden: true });
+
+
+    // Assertion
+
+    expect(heading).toBeInTheDocument()
+    expect(paragraph).toBeInTheDocument()
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', 'https://fetch.com/')
+    expect(button).toBeInTheDocument()
 })
+
